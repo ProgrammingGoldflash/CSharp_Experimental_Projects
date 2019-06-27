b7a84fcce683f99261164ef53c5685229a7deff3
Philip <philip.ainberger@aon.at>
2019-06-27 22:41:35 +0200

    added Projects - HardMonitoring & FileExplorer

---
+++ ExperimentalStuff/HardwareMonitoring/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace HardwareMonitoring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task t = MainAsync(args);
+            t.Wait();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            Monitor monitor = new Monitor();
+            while (true)
+            {
+                var cpu = await monitor.GetCpuUsageForProcess();
+            }
+        }
+
+    }
+
+    class Monitor
+    {
+        public async Task<double> GetCpuUsageForProcess()
+        {
+            var startTime = DateTime.UtcNow;
+            var startCpuUsage = Process.GetCurrentProcess().TotalProcessorTime;
+            await Task.Delay(500);
+
+            var endTime = DateTime.UtcNow;
+            var endCpuUsage = Process.GetCurrentProcess().TotalProcessorTime;
+            var cpuUsedMs = (endCpuUsage - startCpuUsage).TotalMilliseconds;
+            var totalMsPassed = (endTime - startTime).TotalMilliseconds;
+            var cpuUsageTotal = cpuUsedMs / (Environment.ProcessorCount * totalMsPassed);
+            return cpuUsageTotal * 100;
+        }
+    }
+}
